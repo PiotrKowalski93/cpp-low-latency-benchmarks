b9a4194acc00c0df7f5621e13d8256187daabd8c
Piotr Kowalski <piotrkow93@gmail.com>
2026-03-26 23:21:49 +0100

    Percentiles and min,max,avg latency. ToDos for improvements

--- benchmarks/lock_free_ring_buffer/program.cpp
+++ benchmarks/lock_free_ring_buffer/program.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <vector>
 #include <thread>
+#include <algorithm>
+#include <numeric>
 
 #include "queue.h"
 #include "thread_utils.h"
@@ -11,6 +13,9 @@ using namespace Queue;
 // #include <x86intrin.h>
 // uint64_t t = __rdtsc();
 
+// TODO: Add CPU isolation, CPU pinning might not be enought
+// TODO: Add Priority to the thread
+// TODO: Add _mm_pause()?
 int main() {
     
     size_t WARMUP_N_ = 10'000;
@@ -21,6 +26,7 @@ int main() {
     size_t failed_read_ = 0;
 
     std::vector<std::chrono::nanoseconds> latencies_;
+    latencies_.reserve(BENCHMARK_N_);
     
     Queue::LockFreeQueue<Queue::Message> messages_queue_(BENCHMARK_N_);
 
@@ -38,11 +44,12 @@ int main() {
             Message msg;
             msg.data = 1;
             msg.start = std::chrono::steady_clock::now();
+            messages_queue_.write(msg);
 
             // Write to buffer
-            if(!messages_queue_.write(msg)){
-                failed_write_++;
-            }
+            // if(!messages_queue_.write(msg)){
+            //     failed_write_++;
+            // }
         }
 
         production_finished_ = true;
@@ -59,20 +66,14 @@ int main() {
             if(result){
                 // Count latency and save it
                 auto end = std::chrono::steady_clock::now();
-
-                if(!is_warmup){
-                    auto latency = end - msg.start;
-                    latencies_.push_back(latency);
-                }
-            }
-            else {
-                failed_read_++;
+                auto latency = end - msg.start;
+                latencies_.push_back(latency);
             }
         }
     };
 
     // Warmup
-    std::cout << "---------- Warmup ----------" << std::endl;
+    std::cout << "----------- Warmup ------------" << std::endl;
     auto producer_warmup = Common::createAndStartThread(3, "Consumer", consumer_func, true);
     auto consumer_warmup = Common::createAndStartThread(2, "Producer", producer_func, WARMUP_N_);
 
@@ -80,31 +81,40 @@ int main() {
     consumer_warmup->join();
 
     // Benchmark
-    std::cout << "---------- Benchmark ----------" << std::endl;
+    std::cout << "----------- Benchmark -----------" << std::endl;
 
     production_finished_ = false;
+    latencies_.clear();
 
     auto producer_ = Common::createAndStartThread(3, "Consumer", consumer_func, false);
     auto consumer_ = Common::createAndStartThread(2, "Producer", producer_func, BENCHMARK_N_);
 
     producer_->join();
     consumer_->join();
-    
-    std::cout << "Finished benchmark" << std::endl;
-    std::cout << "Latencies count:" << latencies_.size() << std::endl;
+
+    std::cout << "------------ Results -------------" << std::endl;
+
+    sort(latencies_.begin(), latencies_.end());
+    auto sum = std::reduce(latencies_.begin(), latencies_.end());
+    auto avg_latency = sum / latencies_.size();
+
+    std::cout << "Total processed:" << latencies_.size() << std::endl;
     std::cout << "Failed write:" << failed_write_ << std::endl;
 
-    // Print out:
-    // CPUs GHz
-    // min latency
-    // avg latency
-    // max latency
-    // p50
-    // p95
-    // p99
-    // throughput operation per second
-    // total operations
-    // failed writes
+    std::cout << "Min Latency: " << latencies_.front() << std::endl;
+    std::cout << "Avg Latency: " << avg_latency << std::endl;
+    std::cout << "Max Latency: " << latencies_.back() << std::endl;
+
+    auto p50 = latencies_[BENCHMARK_N_ * 0.5];
+    auto p90 = latencies_[BENCHMARK_N_ * 0.9];
+    auto p99 = latencies_[BENCHMARK_N_ * 0.99];
+    auto p999 = latencies_[BENCHMARK_N_ * 0.999];
+
+    std::cout << std::endl;
+    std::cout << "p50: " << p50 << std::endl;
+    std::cout << "p90: " << p90 << std::endl;
+    std::cout << "p99: " << p99 << std::endl;
+    std::cout << "p999: " << p999 << std::endl;
 
     return 0;
 }
